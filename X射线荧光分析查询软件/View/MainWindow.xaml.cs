cb25ca23a6c9f3714d5b6d008287ab0698c6947f
Kira Kira <18628226521@163.com>
2017-12-18 18:23:30 +0800

    forbid DragMove when window is Maximize

--- X射线荧光分析查询软件/View/MainWindow.xaml.cs
+++ X射线荧光分析查询软件/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public static readonly Guid LayoutToken = Guid.NewGuid();
         public INavigationService Nav => ServiceLocator.Current.GetInstance<INavigationService>();
@@ -42,17 +42,16 @@
                 Messenger.Default.Register<bool>(this, LayoutToken, OnMsg);
                 Nav.NavigateTo(nameof(列表视图));
             };
-            // Loaded += (s, e) => Messenger.Default.Register<bool>(this, LayoutToken, OnMsg);
             Unloaded += (s, e) => Messenger.Default.Unregister<bool>(this, LayoutToken);
         }
 
-        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        private void OnMouseDown(object s, MouseButtonEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            if (!_isMaximize && e.LeftButton == MouseButtonState.Pressed)
             {
-                this.DragMove();
+                DragMove();
             }
-            base.OnMouseLeftButtonDown(e);
+            e.Handled = true;
         }
 
         /// <summary>
@@ -61,20 +60,7 @@
         /// <param name="parameter"></param>
         private void OnMsg(bool parameter)
         {
-            if (parameter)
-            {
-                // Row1.Height = GridLength.Auto;
-                // Row2.Height = new GridLength(1, GridUnitType.Star);
-                // NavigationFrame.Visibility = Visibility.Visible;
-                SearchPage.Visibility = Visibility.Collapsed;
-            }
-            else
-            {
-                // Row1.Height = new GridLength(1, GridUnitType.Star);
-                // Row2.Height = GridLength.Auto;
-                // NavigationFrame.Visibility = Visibility.Collapsed;
-                SearchPage.Visibility = Visibility.Visible;
-            }
+            SearchPage.Visibility = parameter ? Visibility.Collapsed : Visibility.Visible;
         }
 
 
@@ -95,37 +81,49 @@
             ((UIElement) s).Clip = rg;
         }
 
-        private void 关于_Click(object sender, RoutedEventArgs e)
+        private void 关于_Click(object s, RoutedEventArgs e)
         {
             new 关于() { Owner = this, WindowStartupLocation = WindowStartupLocation.CenterOwner }.ShowDialog();
         }
 
-        private void 最小化_Click(object sender, RoutedEventArgs e)
+        private void 最小化_Click(object s, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
         }
 
-        private void 最大化_Click(object sender, RoutedEventArgs e)
+        private void 最大化_Click(object s, RoutedEventArgs e)
         {
             if (_isMaximize)
             {
-                Left = _restoreRect.X;
-                Top = _restoreRect.Y;
-                Width = _restoreRect.Width;
-                Height = _restoreRect.Height;
-                _isMaximize = false;
+                RestoreWindow(_restoreRect.X, _restoreRect.Y);
             }
             else
             {
-                _restoreRect = new Rect(Left, Top, Width, Height);
-                Left = SystemParameters.WorkArea.X;
-                Top = SystemParameters.WorkArea.Y;
-                Width = SystemParameters.WorkArea.Width;
-                Height = SystemParameters.WorkArea.Height;
-                _isMaximize = true;
+                MaximizeWindow();
             }
-            最大化.Content = _isMaximize ? "\xE92C" : "\xE92D";
-            最大化.ToolTip = _isMaximize ? "还原" : "最大化";
+        }
+
+        private void MaximizeWindow()
+        {
+            _restoreRect = new Rect(Left, Top, Width, Height);
+            Left = SystemParameters.WorkArea.X;
+            Top = SystemParameters.WorkArea.Y;
+            Width = SystemParameters.WorkArea.Width;
+            Height = SystemParameters.WorkArea.Height;
+            _isMaximize = true;
+            最大化.Content = "\xE92C";
+            最大化.ToolTip = "还原";
+        }
+
+        private void RestoreWindow(double x, double y)
+        {
+            Left = x;
+            Top = y;
+            Width = _restoreRect.Width;
+            Height = _restoreRect.Height;
+            _isMaximize = false;
+            最大化.Content = "\xE92D";
+            最大化.ToolTip = "最大化";
         }
 
         private void 关闭_Click(object sender, RoutedEventArgs e)
